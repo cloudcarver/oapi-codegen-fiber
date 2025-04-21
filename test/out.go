ece7d3891f9ecefbbe02969132a1e2b604ff4bd5
cloudcarver <mikechesterwang@foxmail.com>
2025-04-21 14:27:24 +0800

    add hooks

--- test/out.go
+++ test/out.go
@@ -3,12 +3,15 @@ package apigen
 import "github.com/gofiber/fiber/v2"
 
 type Validator interface { 
+    PreValidate(*fiber.Ctx) error
+    
+    PostValidate(*fiber.Ctx) error
+
     CheckOperationID(c *fiber.Ctx, operationID string) error
 
     OwnCluster(c *fiber.Ctx, userId int32, clusterId int32) error
  
-    GetUserID(c *fiber.Ctx, ) int32
-
+    GetUserID(c *fiber.Ctx) int32
 }
 
 
@@ -27,6 +30,9 @@ func (x *XMiddleware) GetUser(c *fiber.Ctx, clusterId int32) error {
     if c.Get("Authorization") == "" {
 		return c.Status(fiber.StatusUnauthorized).SendString("Authorization header is required")
 	} 
+	if err := x.PreValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
 	operationID := "GetUser"
 	 
 	if err := x.CheckOperationID(c, operationID); err != nil {
@@ -35,14 +41,23 @@ func (x *XMiddleware) GetUser(c *fiber.Ctx, clusterId int32) error {
 	if err := x.OwnCluster(c, x.GetUserID(c), clusterId); err != nil {
 	    return c.Status(fiber.StatusForbidden).SendString(err.Error())
 	}  
+	if err := x.PostValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
     return x.Handler.GetUser(c, clusterId)
 }
 
 // (POST /sign-in)
 func (x *XMiddleware) SignIn(c *fiber.Ctx) error {
     
+	if err := x.PreValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
 	
 	
+	if err := x.PostValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
     return x.Handler.SignIn(c)
 }
 
@@ -51,8 +66,14 @@ func (x *XMiddleware) Test0(c *fiber.Ctx) error {
     if c.Get("Authorization") == "" {
 		return c.Status(fiber.StatusUnauthorized).SendString("Authorization header is required")
 	} 
+	if err := x.PreValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
 	
 	  
+	if err := x.PostValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
     return x.Handler.Test0(c)
 }
 
@@ -61,8 +82,14 @@ func (x *XMiddleware) Test0Post(c *fiber.Ctx) error {
     if c.Get("Authorization") == "" {
 		return c.Status(fiber.StatusUnauthorized).SendString("Authorization header is required")
 	} 
+	if err := x.PreValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
 	
 	    
+	if err := x.PostValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
     return x.Handler.Test0Post(c)
 }
 
@@ -71,8 +98,14 @@ func (x *XMiddleware) GetTest2(c *fiber.Ctx) error {
     if c.Get("Authorization") == "" {
 		return c.Status(fiber.StatusUnauthorized).SendString("Authorization header is required")
 	} 
+	if err := x.PreValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
 	
 	  
+	if err := x.PostValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
     return x.Handler.GetTest2(c)
 }
 
@@ -81,8 +114,14 @@ func (x *XMiddleware) GetTest3(c *fiber.Ctx) error {
     if c.Get("Authorization") == "" {
 		return c.Status(fiber.StatusUnauthorized).SendString("Authorization header is required")
 	} 
+	if err := x.PreValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
 	
 	  
+	if err := x.PostValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
     return x.Handler.GetTest3(c)
 }
 
@@ -91,11 +130,17 @@ func (x *XMiddleware) GetUserId(c *fiber.Ctx, id string) error {
     if c.Get("Authorization") == "" {
 		return c.Status(fiber.StatusUnauthorized).SendString("Authorization header is required")
 	} 
+	if err := x.PreValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
 	operationID := "GetUserId"
 	 
 	if err := x.CheckOperationID(c, operationID); err != nil {
 	    return c.Status(fiber.StatusForbidden).SendString(err.Error())
 	}  
+	if err := x.PostValidate(c); err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
     return x.Handler.GetUserId(c, id)
 }
 
